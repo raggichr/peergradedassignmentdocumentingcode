0ffd6612a6c060f7a1512bd5a51ed67a94ae6ca5
raggichr <raggichr@bluewin.ch>
2020-02-09 19:12:06 +0100

    advanced unit test

--- tests/testthat/test-mytests.R
+++ tests/testthat/test-mytests.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("Test the basic functionality of the package")
+
+library(dplyr)
+library(maps)
+
+setwd(system.file("extdata", package = "peergradedassignmentdocumentingcode"))
+
+test_that("fars_read() works correctly", {
+    expect_is(fars_read("accident_2015.csv.bz2"), "tbl_df")
+    expect_error(fars_read("accident_2020.csv.bz2"))
+})
+
+
+test_that("fars_summarize_years() works correctly", {
+    expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
+    expect_error(fars_summarize_years(2016))
+
 })
